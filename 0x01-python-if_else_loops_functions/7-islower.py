3873e601899374bacc60274ce88fdc7990dfa32b
Daisy_Dawn <agboella20@gmail.com>
2023-06-06 15:40:04 +0100

    task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def islower(c):
-        if len(c) != 1:
-            raise ValueError("Input must be a single character")        
-# Check if the ASCII value of the character is within the range of lowercase letters
-        if ord(c) >= 97 and ord(c) <= 122:
+    if len(c) != 1:
+        raise ValueError("Input must be a single character")
+
+    if ord(c) >= 97 and ord(c) <= 122:
             return True
         else:
             return False
