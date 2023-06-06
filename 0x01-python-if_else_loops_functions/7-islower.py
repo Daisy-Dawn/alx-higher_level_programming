1eb26abd689b36f43f358b89d5763b1bf2e88f38
Daisy_Dawn <agboella20@gmail.com>
2023-06-06 15:43:36 +0100

    task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -5,6 +5,6 @@ def islower(c):
         raise ValueError("Input must be a single character")
 
     if ord(c) >= 97 and ord(c) <= 122:
-            return True
-        else:
-            return False
+        return True
+    else:
+        return False
