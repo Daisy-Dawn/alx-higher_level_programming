0de228bc95a3ad752f8cdf47d34a1ed95fb7b066
Daisy-Dawn <agboella20@gmail.com>
2023-06-11 12:03:01 -0700

    task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    x = len(sentence)
+    y = sentence[0]
+    z = ()
+    if x == 0:
+        z = (0, None)
+    else:
+        z = (x, y)
+    return z
